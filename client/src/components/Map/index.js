7033f0e8042255059f94347481a3600a7af38b7b
Collin Bolles <collinbolles@gmail.com>
2018-12-13 17:48:11 -0500

    Starting to add sidebar

---
+++ client/src/components/Map/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { Body } from "../containers";
+
+class Map extends React.Component {
+
+    render() {
+
+        return (
+            <Body>
+                <p>Hello World!</p>
+            </Body>
+        )
+    }
+
+}
+
+export default Map;
---
+++ client/src/components/Yelp/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Navigator from "../Navigation";
+
+class Yelp extends React.Component {
+    render() {
+        return (
+            <Navigator></Navigator>
+        )
+    }
+}
+
+export default Yelp;
