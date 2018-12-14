57c6ee38e7e988f86d026824a60f396858de1513
Collin Bolles <collinbolles@gmail.com>
2018-12-13 19:13:07 -0500

    Begin modifying navbar into yelp form

--- client/src/components/Sidebar/index.js
+++ client/src/components/Sidebar/index.js
@@ -1,6 +1,6 @@
 import React from "react";
-import SideNav, { Toggle, Nav, NavItem, NavIcon, NavText } from '@trendmicro/react-sidenav';
-import '@trendmicro/react-sidenav/dist/react-sidenav.css';
+import SideNav, { NavItem, NavIcon, NavText } from '@trendmicro/react-sidenav';
+import './style.css';
 
 class Sidebar extends React.Component {
     render() {
@@ -12,23 +12,19 @@ class Sidebar extends React.Component {
                 >
                     <SideNav.Toggle />
                     <SideNav.Nav defaultSelected="home">
-                        <NavItem eventKey="home">
+                        <NavItem>
                             <NavIcon>
                                 <i className="fa fa-fw fa-home" style={{ fontSize: '1.75em' }} />
                             </NavIcon>
-                            <NavText>Home</NavText>
+                            <NavText>
+                                <label>Hello</label>
+                            </NavText>
                         </NavItem>
-                        <NavItem eventKey="charts">
+                        <NavItem>
                             <NavIcon>
                                 <i className="fa fa-fw fa-line-chart" style={{ fontSize: '1.75em' }} />
                             </NavIcon>
                             <NavText>Charts</NavText>
-                        <NavItem eventKey="charts/linechart">
-                            <NavText>Line Chart</NavText>
-                        </NavItem>
-                        <NavItem eventKey="charts/barchart">
-                            <NavText>Bar Chart</NavText>
-                        </NavItem>
                         </NavItem>
                     </SideNav.Nav>
                 </SideNav>
